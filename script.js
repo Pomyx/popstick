ced3a38b3bdc404ee24d18c073e3b81e391b4cf1
Pomyx <199998891+Pomyx@users.noreply.github.com>
2026-05-06 12:13:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  alert("Köszönjük! A feliratkozási rendszer hamarosan aktiválva lesz.");
-});
+const form = document.querySelector("form");
+
+if (form) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Köszönjük! A feliratkozási rendszer hamarosan aktiválva lesz.");
+  });
+}
